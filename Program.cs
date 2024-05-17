19f99dfd803427873ee8704b0095524714efefc2
Raissa Peneluc <raissapeneluc@gmail.com>
2024-05-17 14:59:27 -0300

    deixando o codigo dinamico

--- Program.cs
+++ Program.cs
@@ -1,8 +1,22 @@
 ﻿using DesafioPOO.Models;
 
-Console.WriteLine("Especificações Nokia:");
+Console.WriteLine("Digite o número do celular Nokia:");
+string numeroNokia = Console.ReadLine();
+
+Console.WriteLine("Digite o modelo do celular Nokia:");
+string modeloNokia = Console.ReadLine();
+
+Console.WriteLine("Digite o IMEI do celular Nokia:");
+string imeiNokia = Console.ReadLine();
+
+Console.WriteLine("Digite a memória do celular Nokia:");
+int memoriaNokia = int.Parse(Console.ReadLine());
+
+Nokia n1 = new Nokia(numero: numeroNokia, modelo: modeloNokia, imei: imeiNokia, memoria: memoriaNokia);
+
+
+Console.WriteLine("\n\nEspecificações Nokia:");
 Console.WriteLine("---------------------");
-Nokia n1 = new Nokia(numero: "(71) 93456-9786", modelo: "S20", imei:"343423523", memoria: 256);
 Console.WriteLine($"Número: {n1.Numero}");
 Console.WriteLine($"Modelo: {n1.Modelo}");
 Console.WriteLine($"IMEI do Smartphone: {n1.IMEI}");
@@ -16,9 +30,23 @@ n1.ReceberLigacao();
 
 Console.WriteLine("\n");
 
-Console.WriteLine("Especificações Iphone:");
+Console.WriteLine("Digite o número do celular Iphone:");
+string numeroIphone = Console.ReadLine();
+
+Console.WriteLine("Digite o modelo do celular Iphone:");
+string modeloIphone = Console.ReadLine();
+
+Console.WriteLine("Digite o IMEI do celular Iphone:");
+string imeiIphone = Console.ReadLine();
+
+Console.WriteLine("Digite a memória do celular Iphone:");
+int memoriaIphone = int.Parse(Console.ReadLine());
+
+Iphone i1 = new Iphone(numero: numeroIphone, modelo: modeloIphone, imei: imeiIphone, memoria: memoriaIphone);
+
+
+Console.WriteLine("\n\nEspecificações Iphone:");
 Console.WriteLine("---------------------");
-Iphone i1 = new Iphone(numero: "(71) 92368-1234", modelo: "15 plus", imei:"12343536", memoria: 128);
 Console.WriteLine($"Número: {i1.Numero}");
 Console.WriteLine($"Modelo: {i1.Modelo}");
 Console.WriteLine($"IMEI do Smartphone: {i1.IMEI}");
